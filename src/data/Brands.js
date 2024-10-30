83157fb1d8014ce68fe1a34eec0be4187cbebf37
sadiqkhan3006 <sadiqkhan3006@gmail.com>
2024-10-30 23:06:21 +0530

    making trusted by page responsive

--- src/data/Brands.js
+++ src/data/Brands.js
@@ -2,6 +2,10 @@ import brand0logo from "../assets/brands/brand0/brand0logo.svg";
 import brand0img1 from "../assets/brands/brand0/brand0img1.png";
 import brand0img2 from "../assets/brands/brand0/brand0img2.png";
 import brand0img3 from "../assets/brands/brand0/brand0img3.png";
+import brand1logo from "../assets/brands/brand1/brand1logo.svg";
+import brand1img1 from "../assets/brands/brand1/brand1img1.png";
+import brand1img2 from "../assets/brands/brand1/brand1img2.png";
+import brand1img3 from "../assets/brands/brand1/brand1img3.png";
 const brands = [
   {
     id: 0,
@@ -31,34 +35,34 @@ const brands = [
       },
     ],
   },
-  //   {
-  //     id: 1,
-  //     name: "Aeigis",
-  //     logo: brand0logo,
-  //     desc: "A trusted partner in natural wellness, offering safe supplements to support your overall health and vitality. ",
-  //     instagram: "www.instagram.com",
-  //     website: "wwww.instagram.com",
-  //     products: [
-  //       {
-  //         id: 0,
-  //         title: "Daily Detox Shot",
-  //         img: brand0img1,
-  //         link: "..",
-  //       },
-  //       {
-  //         id: 1,
-  //         title: "Pre Workout Shot",
-  //         img: brand0img2,
-  //         link: "..",
-  //       },
-  //       {
-  //         id: 2,
-  //         title: "Green Tea Tablets",
-  //         img: brand0img3,
-  //         link: "..",
-  //       },
-  //     ],
-  //   },
+  {
+    id: 1,
+    name: "Hempbuti",
+    logo: brand1logo,
+    desc: "Whether you're an athlete, professional, senior, or simply seeking self-care, Hempbuti offers solutions for enhanced well-being.",
+    instagram: "https://www.instagram.com/hempbuti/",
+    website: "https://www.hempbuti.in/",
+    products: [
+      {
+        id: 0,
+        title: "Ritual Oil",
+        img: brand1img1,
+        link: "https://www.hempbuti.in/product-detail/ritual-oil-30-ml",
+      },
+      {
+        id: 1,
+        title: "Over-Parent Drops",
+        img: brand1img2,
+        link: "https://www.hempbuti.in/product-detail/over-parent-supplement-drops-30-ml-for-small-pets",
+      },
+      {
+        id: 2,
+        title: "Rx Migraine Aid",
+        img: brand1img3,
+        link: "https://www.hempbuti.in/product-detail/rx-migraine-aid-30-ml",
+      },
+    ],
+  },
 ];
 
 export default brands;
--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -27,69 +27,79 @@ function Brands() {
       </div>
       <div className="flex flex-col gap-y-8 mt-[35px]">
         {brands.map((item) => (
-          <div className="flex flex-row items-center">
-            <div className="flex flex-col w-[70%] gap-y-[16px] ">
-              <div className="flex flex-row gap-[24px] px-[24px]">
-                <div className="w-[60%]">
-                  <div>
-                    <img src={item.logo} />
+          <div className="flex flex-col lg:flex-row gap-y-6  items-center">
+            <div className="flex flex-col md:flex-row lg:flex-col lg:w-[70%] gap-y-[16px]  items-start ">
+              <div className="flex items-center lg:items-start md:flex-row gap-x-[8px] md:gap-x-[16px] lg:gap-x-[24px] md:px-[24px]">
+                <div className="md:w-[60%]">
+                  <div className="flex items-start">
+                    <img className="w-[48px] md:w-full" src={item.logo} />
                   </div>
                 </div>
                 <div className="flex flex-col gap-2">
-                  <div className="font-Poppins text-black text-[32px] leading-[36px] font-medium ">
+                  <div className="font-Poppins text-black text-[18px] leading-[22px] md:text-[20px] md:leading-[24px]  lg:text-[32px] lg:leading-[36px] font-medium ">
                     {item.name}
                   </div>
-                  <div className="font-Poppins text-[#7D7D7D] text-[24px] leading-[34px] font-normal ">
+                  <div className=" hidden  md:flex font-Poppins text-[#7D7D7D] text-[16px] leading-[18px] md:text-[18px] md:leading-[22px]  lg:text-[24px] lg:leading-[34px]  font-normal ">
                     {item.desc}
                   </div>
                 </div>
               </div>
-              <div className="flex flex-row px-[24px] gap-x-4">
-                <div
-                  onClick={() => {
-                    window.open(item.instagram, "_blank");
-                  }}
-                  className="cursor-pointer  px-[20px] py-[10px] bg-[#F8F8F7] border border-[#E4E2DE] rounded-[30px] flex flex-row items-center gap-x-2 "
-                >
-                  <div>
-                    <FaInstagram />
-                  </div>
-                  <div className="font-Archivo  text-black text-[15px]  font-medium">
-                    Instagram
+              <div className=" md:hidden font-Poppins text-[#7D7D7D] text-[16px] leading-[18px] md:text-[18px] md:leading-[22px]  lg:text-[24px] lg:leading-[34px]  font-normal ">
+                {item.desc}
+              </div>
+              <div className=" flex flex-row md:flex-col gap-y-2 lg:flex-row lg:px-[24px] gap-x-4">
+                <div>
+                  <div
+                    onClick={() => {
+                      window.open(item.instagram, "_blank");
+                    }}
+                    className="cursor-pointer  px-[20px] py-[8px] md:py-[10px] bg-[#F8F8F7] border border-[#E4E2DE] rounded-[30px] flex flex-row items-center gap-x-2 "
+                  >
+                    <div>
+                      <FaInstagram />
+                    </div>
+                    <div className="font-Archivo  text-black text-[15px]  font-medium">
+                      Instagram
+                    </div>
                   </div>
                 </div>
-                <div
-                  onClick={() => {
-                    window.open(item.website, "_blank");
-                  }}
-                  className="cursor-pointer  px-[20px] py-[10px] bg-[#F8F8F7] border border-[#E4E2DE] rounded-[30px] flex flex-row items-center gap-x-2 "
-                >
-                  <div>
-                    <CiGlobe />
-                  </div>
-                  <div className="font-Archivo  text-black text-[15px]  font-medium">
-                    Website
+                <div>
+                  <div
+                    onClick={() => {
+                      window.open(item.website, "_blank");
+                    }}
+                    className="cursor-pointer  px-[20px] py-[8px] md:py-[10px] bg-[#F8F8F7] border border-[#E4E2DE] rounded-[30px] flex flex-row items-center gap-x-2 "
+                  >
+                    <div>
+                      <CiGlobe />
+                    </div>
+                    <div className="font-Archivo  text-black text-[15px]  font-medium">
+                      Website
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="flex flex-row py-[32px] px-[2%] rounded-[32px] bg-[#F8F8F7] w-full justify-evenly ">
+            <div className="flex flex-row py-[20px] md:py-[24px] lg:py-[32px] px-[2%] rounded-[16px] md:rounded-[32px] bg-[#F8F8F7] w-full gap-x-[8px] md:justify-evenly ">
               {item.products.map((prod) => (
                 <div
                   key={prod.id}
-                  className=" relative pt-[8px] px-[8px] pb-[12px]  border-[1px] border-[#E4E2DE]  rounded-3xl    "
+                  className=" relative  pb-[12px]  border-[1px] border-[#E4E2DE] rounded-[16px]  md:rounded-3xl    "
                 >
                   <div>
-                    <img src={prod.img} />
+                    <img
+                      className="aspect-auto lg:h-full   lg:w-full"
+                      src={prod.img}
+                    />
                   </div>
-                  <div className="font-Archivo text-black font-semibold text-[20px] text-center   ">
+                  <div className=" pt-[8px] px-[8px] font-Archivo text-black font-semibold text-[12px] md:text-[16px] lg:text-[20px] text-center   ">
                     {prod.title}
                   </div>
                   <button
                     onClick={() => {
                       window.open(prod.link, "_blank");
                     }}
-                    className="absolute right-0 bottom-[20%] bg-[#DA482C] text-white w-[45px] h-[45px] rounded-full flex items-center justify-center "
+                    className="absolute right-0 bottom-[32%] md:bottom-[20%] bg-[#DA482C] text-white w-[32px] h-[32px] md:w-[32px] md:h-[32px] lg:w-[45px] lg:h-[45px] rounded-full flex items-center justify-center "
                   >
                     <MdArrowOutward />
                   </button>
